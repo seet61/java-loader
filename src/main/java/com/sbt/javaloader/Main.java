d391caf089b98895a16084175ac74dc6273d5938
seet61 <seet_seet@mail.ru>
2018-06-01 15:57:02 +0300

    add variant with success load api class

--- src/main/java/com/sbt/javaloader/Main.java
+++ src/main/java/com/sbt/javaloader/Main.java
@@ -4,9 +4,9 @@ import com.sbt.javaloader.api.ApiClassloader;
 
 public class Main {
     public static void main(String[] args) {
-        ApiClassloader apiClassloader = new ApiClassloader();
+        ApiClassloader apiClassloader = new ApiClassloader(ClassLoader.getSystemClassLoader());
         try {
-            Class<?> icalc = apiClassloader.findClass("ICalculator");
+            Class<?> icalc = apiClassloader.findClass("com.sbt.javaloader.api.ICalculator");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
--- src/main/java/com/sbt/javaloader/api/ApiClassloader.java
+++ src/main/java/com/sbt/javaloader/api/ApiClassloader.java
@@ -7,11 +7,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class ApiClassloader extends ClassLoader{
+public class ApiClassloader extends ClassLoader {
     //путь для загрузки
-    private String pathToBin = "C:\\Projects\\java-loader\\target\\classes\\com\\sbt\\javaloader\\api";
+    private String pathToBin = "target\\classes";
 
-    public ApiClassloader() {
+    public ApiClassloader(ClassLoader parent) {
+        super(parent);
     }
 
     public Class<?> findClass(String className) throws ClassNotFoundException {
@@ -19,7 +20,8 @@ public class ApiClassloader extends ClassLoader{
             /**
              * Получем байт-код из файла и загружаем класс в рантайм
              */
-            File f = new File(pathToBin, className + ".class");
+            ///https://www.concretepage.com/java/custom-classloader-java-example
+            File f = new File(pathToBin, className.replace(".", "/") + ".class");
             System.out.println(f.toString());
             byte b[] = loadClassData(f.toString());
             return defineClass(className, b, 0, b.length);
@@ -29,41 +31,6 @@ public class ApiClassloader extends ClassLoader{
             return super.findClass(className);
         }
     }
-
-    /*
-    //http://www.ilnurgi1.ru/docs/java/java/lang/ClassLoader.html
-    private byte[] fetchClassFromFS(String path) throws FileNotFoundException, IOException {
-        InputStream is = new FileInputStream(new File(path));
-
-        // Get the size of the file
-        long length = new File(path).length();
-
-        if (length > Integer.MAX_VALUE) {
-            // File is too large
-        }
-
-        // Create the byte array to hold the data
-        byte[] bytes = new byte[(int)length];
-
-        // Read in the bytes
-        int offset = 0;
-        int numRead = 0;
-        while (offset < bytes.length
-                && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
-            offset += numRead;
-        }
-
-        // Ensure all the bytes have been read in
-        if (offset < bytes.length) {
-            throw new IOException("Could not completely read file "+path);
-        }
-
-        // Close the input stream and return bytes
-        is.close();
-        return bytes;
-
-    }
-    */
 
     private byte[] loadClassData(String name) throws FileNotFoundException, IOException {
         // load the class data from the connection
